78ad125c2b9bb7849897a0fb98b5559aa040964c
Muhammad Iqbal <miqbal020@gmail.com>
2023-09-06 00:29:47 +0700

    Add Query Parameter

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -28,11 +30,33 @@ async def delete():
     return {"message": "DELETE Methods"}
 
 
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+
+@app.get('/foods/{food_name}')
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "message": "Vegetables"}
+
+    if food_name.value == "fruits":
+        return {"food_name": food_name, "message": "Fruits"}
+
+    return {"food_name": food_name, "message": "Dairy"}
+
+
+fake_items_db = [
+    {"item_name": "Foo"}, {"item_name": "Foo2"}, {"item_name": "Foo3"}, {"item_name": "Foo4"}
+]
+
+
 @app.get('/items', description='Item List')
-async def get_item():
-    return {"message": "Item List"}
+async def get_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
 
 
 @app.get('/items/{item_id}', description='Get Item by ID')
-async def get_item_id(item_id: int):
+async def get_item_id(item_id: str):
     return {"message": item_id}
